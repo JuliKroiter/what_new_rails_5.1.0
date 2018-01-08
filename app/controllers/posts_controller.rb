e419e10de92237d1affd4dba562702b2a6b5ebbf
JuliKroiter <kroiter09@gmail.com>
2018-01-08 09:57:25 +0200

    Fix permit params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,44 +2,32 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   respond_to :json, :html
 
-  # GET /posts
-  # GET /posts.json
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
   def show
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
   end
 
-  # POST /posts
-  # POST /posts.json
   def create
     @post = Post.create(post_params)
 
     respond_with(@post, location: @post)
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
   def update
     @post.update(post_params)
 
     respond_with(@post, location: @post)
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
     @post.destroy
 
@@ -47,13 +35,11 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def post_params
-      params.fetch(:post, {})
-    end
+  def post_params
+    params.require(:post).permit(:title, :boby, :user)
+  end
 end
